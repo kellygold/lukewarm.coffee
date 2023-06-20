a5010a9ca4949c2a21e0d697a992f4d4fa7eec8c
kellygold <kellytgold@gmail.com>
2023-06-20 08:12:17 -0600

    Update products.js

--- public/scripts/products.js
+++ public/scripts/products.js
@@ -2,7 +2,7 @@
 var express = require('express');
 var router = express.Router();
 const Alloy = require("alloy-node");
-const alloy = new Alloy("nFFNapI8Qh-9YfR_oGCN3");
+const alloy = new Alloy("{{api_key}}");
 
 router.get('/', (req, res) => {
     res.render('products');
