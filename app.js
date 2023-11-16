98cba6445dd8ea2d8648a09377c810cf2ad5ee60
Filipe Soares <filipe.afns@gmail.com>
2023-11-15 22:01:55 -0300

    Adds post processing to project

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var sassMiddleware = require('node-sass-middleware');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -11,9 +12,18 @@ var contactsRouter = require('./routes/contacts');
 var webhooksRouter = require('./routes/webhooks');
 var invoicesRouter = require('./routes/invoices');
 
-var app = express();
-// set the view engine to html
-app.use(express.static(__dirname + '/public'));
+var app = express();  // Move this line up
+
+// Compile SCSS to CSS using node-sass middleware
+app.use(sassMiddleware({
+  src: path.join(__dirname, 'public', 'stylesheets'),
+  dest: path.join(__dirname, 'public', 'css'),
+  debug: true,
+  outputStyle: 'compressed',
+  prefix: '/css'  // Set the path prefix for generated CSS files
+}));
+
+// set the view engine to ejs
 app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(express.json());
